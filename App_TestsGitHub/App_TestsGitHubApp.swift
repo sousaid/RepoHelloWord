509bc7ecb428684972ce7b73ffa9624254f895c7
sousaid <sousaid.abdellah1999@gmail.com>
2024-05-28 19:43:55 +0100

    Initial Commit

---
+++ App_TestsGitHub/App_TestsGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  App_TestsGitHubApp.swift
+//  App_TestsGitHub
+//
+//  Created by Adria on 28/5/2024.
+//
+
+import SwiftUI
+
+@main
+struct App_TestsGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
